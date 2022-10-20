c30d29f7b212aabc19ccaea7b68372e19298e4c8
Fault1err <oalkozlova@gmail.com>
2022-10-20 20:41:39 +0300

    update2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@
 import './App.css';
 
 function App(props) {
-  const topPosition = '40px';
-  return (
+   return (
     <div className="App">
-      <header className="App-header" style = {{top: topPosition}}>
+      <header className="App-header">
         My First React App
-        <h3> Hello, {props.name} </h3>
+        <h3> Hello, {props.name}</h3>
       </header>
     </div>
   );
