487637a07f89fa51b6b4805324786317c8816efa
Fault1err <oalkozlova@gmail.com>
2022-10-20 19:50:23 +0300

    lesson_1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  const topPosition = '40px';
+  return (
+    <div className="App">
+      <header className="App-header" style = {{top: topPosition}}>
+        My First React App
+        <h3>Hello, {props.name} </h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const myName = 'Olga';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App name={myName} />
+  </React.StrictMode>,
+
+  document.getElementById("root")
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
