7aa8cf01c547ade207efc24dc16f5e827684d985
Fault1err <oalkozlova@gmail.com>
2022-10-20 20:35:08 +0300

    update1

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const myName = 'Olga';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+const myName = 'Olga';
 
 ReactDOM.render(
   <React.StrictMode>
