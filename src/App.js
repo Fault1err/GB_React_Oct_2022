af793e682062f6b7e79a31df40329c7903cd3c91
Fault1err <oalkozlova@gmail.com>
2022-10-20 21:31:26 +0300

    update3

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 import './App.css';
 
 function App(props) {
-   return (
+    return (
     <div className="App">
       <header className="App-header">
         My First React App
-        <h3> Hello, {props.name}</h3>
+        <h3 className='Hello'> Hello, {props.name}</h3>
       </header>
     </div>
   );
